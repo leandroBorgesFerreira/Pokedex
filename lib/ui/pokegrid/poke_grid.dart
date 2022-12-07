0a2ad7280bb7ac5921d9f83da625a44f7380c57d
Leandro Borges Ferreira <lehen01@gmail.com>
2022-12-07 22:04:13 +0100

    Adding a decoration

--- lib/ui/pokegrid/poke_grid.dart
+++ lib/ui/pokegrid/poke_grid.dart
@@ -1,47 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
-
 import 'package:pokedex/model/pokemon_info.dart';
+import 'package:palette_generator/palette_generator.dart';
 
-StatelessWidget pokemonTile(Color? color, String name, int imageIndex) {
+StatelessWidget pokemonTile(
+    Color? color, String name, String type, int imageIndex) {
   const _nameFont =
       TextStyle(fontSize: 18, color: Colors.white, fontWeight: FontWeight.bold);
 
   const _typeFont =
       TextStyle(fontSize: 10, color: Colors.white, fontWeight: FontWeight.bold);
 
-  var nameWidget = Padding(
+  var _nameWidget = Padding(
     padding: const EdgeInsets.symmetric(vertical: 5),
     child: Text(name, style: _nameFont),
   );
 
-  var elementWidget = Row(children: [
+  var _elementWidget = Row(children: [
     Container(
-      child: Center(child: Text(name, style: _typeFont)),
+      child: Center(child: Text(type, style: _typeFont)),
       decoration: const BoxDecoration(
-          color: Colors.white24,
-          borderRadius: BorderRadius.all(Radius.circular(20))),
+        color: Colors.white24,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 6),
     )
   ]);
 
-  var pictureWidget = Image.network(
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$imageIndex.png",
-    width: 90,
-    // height: 100,
+  var _pokemonInfo = ListView(
+    physics: const NeverScrollableScrollPhysics(),
+    children: <Widget>[_nameWidget, _elementWidget],
   );
 
-  var pokemonInfo = ListView(children: <Widget>[nameWidget, elementWidget]);
+  var _decoImage = OverflowBox(
+    maxHeight: 270,
+    child: Image.asset(
+      "assets/pokeball_grey.png",
+      height: 270,
+    ),
+    alignment: AlignmentDirectional.center,
+  );
+
+  var _pictureWidget = Container(
+    padding: const EdgeInsets.only(left: 0, right: 8, top: 0, bottom: 4),
+    child: Image.network(
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$imageIndex.png",
+      width: 100,
+    ),
+  );
 
   var tileForeground = Stack(
-    children: [pokemonInfo, pictureWidget],
+    children: [_decoImage, _pokemonInfo, _pictureWidget],
     alignment: AlignmentDirectional.bottomEnd,
   );
 
   return Container(
-    padding: const EdgeInsets.only(left: 16, right: 8, top: 12, bottom: 8),
+    clipBehavior: Clip.hardEdge,
+    padding: const EdgeInsets.only(left: 16, right: 0, top: 12, bottom: 0),
     margin: const EdgeInsets.all(6),
     decoration: BoxDecoration(
         color: color,
@@ -59,9 +75,7 @@ class PokemonGrid extends StatefulWidget {
 
 class _PokemonGridState extends State<PokemonGrid> {
   late int _offset;
-  late bool _loading;
   final int _limit = 20;
-  final int _nextPageTrigger = 3;
   late List<PokemonInfo> _pokemonList;
   late ScrollController _scrollController;
 
@@ -71,7 +85,6 @@ class _PokemonGridState extends State<PokemonGrid> {
       var nextPageTrigger = 0.8 * _scrollController.position.maxScrollExtent;
 
       if (_scrollController.position.pixels > nextPageTrigger) {
-        _loading = true;
         fetchData();
       }
     });
@@ -108,7 +121,7 @@ class _PokemonGridState extends State<PokemonGrid> {
       itemCount: _pokemonList.length,
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
-        childAspectRatio: 1.3,
+        childAspectRatio: 1.4,
       ),
       itemBuilder: (BuildContext context, int index) {
         if (index >= _pokemonList.length - 1) {
@@ -123,6 +136,7 @@ class _PokemonGridState extends State<PokemonGrid> {
           return pokemonTile(
             _colors.elementAt(index % _colors.length),
             pokemon.name,
+            pokemon.type,
             index + 1,
           );
         }
@@ -135,7 +149,6 @@ class _PokemonGridState extends State<PokemonGrid> {
     super.initState();
 
     _offset = 0;
-    _loading = false;
     _pokemonList = [];
     _scrollController = ScrollController();
 
@@ -156,15 +169,11 @@ class _PokemonGridState extends State<PokemonGrid> {
 
       _pokemonList.addAll(pokemonList);
       setState(() {
-        _loading = false;
         _offset += _limit;
         _pokemonList = _pokemonList;
       });
     } catch (e) {
-      print("error --> $e");
-      setState(() {
-        _loading = false;
-      });
+      setState(() {});
     }
   }
 }
